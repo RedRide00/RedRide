ad6ce76048ca6781dd11e8655a8b67043b9f87a3
RedRide00 <atifioussama19@gmail.com>
2025-04-12 15:13:41 +0100

    src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1>🚕 RedRide is live! Welcome to Casablanca Taxi Estimator 🚖</h1>;
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
